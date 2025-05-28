14cef793feca2fb1d505437f811fa3b0fb6bde5e
박관우 <pgw4712@icloud.com>
2025-05-28 23:27:37 +0900

    간단한 수정
    디렉토리 구조
    클래스 네임

--- src/main/java/dev/park/dailynews/service/SubjectService.java
+++ src/main/java/dev/park/dailynews/service/SubjectService.java
@@ -1,10 +1,9 @@
 package dev.park.dailynews.service;
 
-import dev.park.dailynews.domain.news.Subject;
+import dev.park.dailynews.domain.subject.Subject;
 import dev.park.dailynews.domain.user.User;
 import dev.park.dailynews.dto.request.SubjectRequest;
-import dev.park.dailynews.dto.response.anthropic.SubjectResponse;
-import dev.park.dailynews.exception.UserNotFoundException;
+import dev.park.dailynews.dto.response.subject.SubjectResponse;
 import dev.park.dailynews.model.LoginUserContext;
 import dev.park.dailynews.repository.subject.SubjectRepository;
 import dev.park.dailynews.repository.user.UserRepository;
