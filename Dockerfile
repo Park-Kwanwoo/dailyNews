50d6ccea58f0d73c333b6be452ee40a785b5642a
박관우 <pgw4712@icloud.com>
2025-06-04 19:38:21 +0900

    github actions CD 구축

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM eclipse-temurin:17-jdk
 ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
-ENV PROFILE=prod
-ENTRYPOINT ["java", "-Dspring.active.profiles=${PROFILE}", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-set -e
-
-ERR_MSG=''
-
-trap 'echo "Error occurred: $ERR_MSG. Exiting deploy script."; exit 1' ERR
-
 if [ -f .env ]; then
     source .env
     echo "환경 변수 로드"
@@ -14,5 +7,34 @@ else
   exit 1
 fi
 
-ssh -i ~/.ssh/dailynews.pem ec2-user@$EC2_IP
-mkdir success
+ssh -T -i ~/.ssh/dailynews.pem ec2-user@$EC2_IP \
+ "DOCKER_USERNAME=$DOCKER_USERNAME DOCKER_PASSWORD=$DOCKER_PASSWORD bash -s" << 'EOF'
+
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin \
+
+if docker ps --filter "name=dailynews-blue" --quiet | grep -E .; then
+  RUN_TARGET="dailynews-green"
+  STOP_TARGET="dailynews-blue"
+  WAS_RUN_PORT=8081
+  WAS_STOP_PORT=8080
+
+else
+  RUN_TARGET="dailynews-blue"
+  STOP_TARGET="dailynews-green"
+  WAS_RUN_PORT=8080
+  WAS_STOP_PORT=8081
+fi
+
+docker run -d -p $WAS_RUN_PORT:8080 --name $RUN_TARGET pgw4712/dailynews:latest
+docker logout
+
+sleep 10
+STOP_CONTAINER_ID=$(docker ps --filter "name=$STOP_TARGET" --quiet)
+
+if [ -n $STOP_CONTAINER_ID ]; then
+  docker stop $STOP_CONTAINER_ID
+  docker rm $STOP_CONTAINER_ID
+fi
+
+sudo systemctl reload nginx
+EOF
