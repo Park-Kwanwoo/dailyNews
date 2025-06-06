94ee17e4b5694a0630aa4b74ae1f9afe256a77e5
박관우 <pgw4712@icloud.com>
2025-06-06 22:36:26 +0900

    정적 리소스 접근 추가

--- src/main/java/dev/park/dailynews/config/WebMvcConfig.java
+++ src/main/java/dev/park/dailynews/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -21,10 +22,22 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor)
-                .excludePathPatterns("/**")
-                .excludePathPatterns("/token/reissue")
-                .excludePathPatterns("/social/login")
-                .excludePathPatterns("/error");
+                .excludePathPatterns(
+                        "/token/reissue",
+                        "/social/login",
+                        "/error",
+                        "/",
+                        "/index.html",
+                        "/favicon.ico",
+                        "/static/**",
+                        "/css/**",
+                        "/js/**",
+                        "/img/**",
+                        "/assets/**",
+                        "/*.js",
+                        "/*.css",
+                        "/*.ico"
+                );
     }
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
