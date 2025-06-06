9d830d9e95e035bbf2d85399ac96f3d720f19c2f
박관우 <pgw4712@icloud.com>
2025-06-06 23:14:22 +0900

    크론잡 스케쥴링 수정

--- src/main/java/dev/park/dailynews/controller/NewsController.java
+++ src/main/java/dev/park/dailynews/controller/NewsController.java
@@ -23,7 +23,7 @@ public class NewsController {
 
     private final NewsService newsService;
 
-    @Scheduled(cron = "0 8 * * *")
+    @Scheduled(cron = "0 8 * * * *")
     public void generateNews() {
         newsService.issueAndStoreNews();
     }
