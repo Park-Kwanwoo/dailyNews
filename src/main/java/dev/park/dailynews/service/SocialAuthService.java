2078a843fbec438ca611abb6ea54d9a188cf155e
박관우 <pgw4712@icloud.com>
2025-06-13 21:34:55 +0900

    로그아웃 API 수정
    - 토큰 유효성 검증 추가

--- src/main/java/dev/park/dailynews/service/SocialAuthService.java
+++ src/main/java/dev/park/dailynews/service/SocialAuthService.java
@@ -4,10 +4,14 @@ import dev.park.dailynews.domain.user.User;
 import dev.park.dailynews.dto.response.sosical.SocialLoginParams;
 import dev.park.dailynews.dto.response.sosical.SocialLogoutParams;
 import dev.park.dailynews.dto.response.token.TokenResponse;
+import dev.park.dailynews.exception.ExpiredTokenException;
+import dev.park.dailynews.exception.InvalidTokenException;
 import dev.park.dailynews.infra.auth.jwt.JwtUtils;
 import dev.park.dailynews.model.SocialUserInfoContext;
 import dev.park.dailynews.model.UserContext;
 import dev.park.dailynews.repository.user.UserRepository;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -25,10 +29,11 @@ public class SocialAuthService {
 
     public UserContext findOrCreateUser(SocialUserInfoContext userInfo) {
 
-        userRepository.findByEmail(userInfo.getEmail())
+        User savedUser = userRepository.findByEmail(userInfo.getEmail())
                 .orElseGet(() -> createUser(userInfo));
 
-        return UserContext.from(userInfo);
+        String socialToken = userInfo.getSocialToken();
+        return UserContext.from(savedUser, socialToken);
     }
 
     public User createUser(SocialUserInfoContext socialUserInfo) {
@@ -54,13 +59,19 @@ public class SocialAuthService {
 
     public void logout(String accessToken) {
 
-        redisTemplate.opsForValue()
-                .set(accessToken, "Blacklist", jwtUtils.extractExpiration(accessToken), TimeUnit.SECONDS);
+        try {
+            String socialToken = jwtUtils.extractSocialToken(accessToken);
+            String provider = jwtUtils.extractProvider(accessToken);
 
-        String socialToken = jwtUtils.extractSocialToken(accessToken);
-        String provider = jwtUtils.extractProvider(accessToken);
+            redisTemplate.opsForValue()
+                    .set(accessToken, "Blacklist", jwtUtils.extractExpiration(accessToken), TimeUnit.SECONDS);
 
-        SocialLogoutParams params = SocialLogoutParams.from(socialToken, provider);
-        clientService.logout(params);
+            SocialLogoutParams params = SocialLogoutParams.from(socialToken, provider);
+            clientService.logout(params);
+        } catch (ExpiredJwtException e) {
+            throw new ExpiredTokenException(e.getMessage());
+        } catch (JwtException e) {
+            throw new InvalidTokenException();
+        }
     }
 }
