598f0bbe66112a596c1a18963a3a95d11470727b
박관우 <pgw4712@icloud.com>
2025-06-13 21:37:59 +0900

    RestTemplate 수정
    - 타임아웃 시간 설정

--- src/main/java/dev/park/dailynews/config/DailyNewsConfig.java
+++ src/main/java/dev/park/dailynews/config/DailyNewsConfig.java
@@ -1,16 +1,22 @@
 package dev.park.dailynews.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class DailyNewsConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build();
     }
 
     @Bean
