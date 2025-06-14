53ef698ecc6689dff4e35db1e7852a90cbdc4d6a
박관우 <pgw4712@icloud.com>
2025-06-14 23:27:27 +0900

    ExceptionController 수정
    - RestTemplateException Handler 구현
    - HttpRequestMethodNotSupportedException Hanlder 구현

--- src/main/java/dev/park/dailynews/exception/DailyNewsExceptionController.java
+++ src/main/java/dev/park/dailynews/exception/DailyNewsExceptionController.java
@@ -1,10 +1,16 @@
 package dev.park.dailynews.exception;
 
 import dev.park.dailynews.dto.response.common.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.RestClientException;
 
+@Slf4j
 @RestControllerAdvice
 public class DailyNewsExceptionController {
 
@@ -16,5 +22,19 @@ public class DailyNewsExceptionController {
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ApiResponse<?> validExceptionHandler(MethodArgumentNotValidException e) {
         return ApiResponse.errorWithBindingResult(e);
+    }
+
+    @ExceptionHandler(value = RestClientException.class)
+    public ApiResponse<?> restTemplateExceptionHandler(RestClientException e) {
+        return ApiResponse.error(e.getMessage());
+    }
+
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public void restTemplateExceptionHandler(HttpRequestMethodNotSupportedException ex,
+                                                       HttpServletRequest req) {
+        log.error("Unsupported HTTP Method: {} {} from IP: {}", req.getMethod(),
+                req.getRequestURI(),
+                req.getRemoteAddr());
+        log.error("{}", ex.getMessage());
     }
 }
