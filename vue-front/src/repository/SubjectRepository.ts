fca6b7735e4a745764e50eba360b5f101dcb8772
박관우 <pgw4712@icloud.com>
2025-07-10 16:52:07 +0900

    hotfix: SubjectRespository 수정
    - 주제 부적합 응답 시 알림창 생성

--- vue-front/src/repository/SubjectRepository.ts
+++ vue-front/src/repository/SubjectRepository.ts
@@ -22,7 +22,7 @@ export default class SubjectRepository {
       .then((r) => {
         const statusCode = r.statusCode
         if (statusCode == 'SUCCESS') router.go(0)
-        else {
+        else if (statusCode == 'ERROR') {
           ElMessage.error(r.message)
         }
       })
@@ -61,6 +61,9 @@ export default class SubjectRepository {
       .then((r) => {
         const statusCode = r.statusCode
         if (statusCode == 'SUCCESS') router.go(0)
+        else if (statusCode == 'ERROR') {
+          ElMessage.error(r.message)
+        }
       })
       .catch((e) => {
         ElMessage.error('주제가 수정되지 않았습니다. 다시 시도해주세요')
